d4ecd44f8e42d1757aba6bce8cf62057af2a2348
yusangeng <yusangeng@outlook.com>
2019-02-05 01:42:11 +0800

    fix help information

--- src/actions/help.ts
+++ src/actions/help.ts
@@ -5,11 +5,11 @@ Usage: pki <command> <...parameters>
 
 where <command> is one of:
     help: Get help information.
-    add: Add a github repo as project template.
+    add: Add a github repo archive url as project template.
     init: Initialize project in an empty folder.
 
 Example:
-    pki add tslib https://github.com/yusangeng/packi-template-tslib
+    pki add tslib https://github.com/yusangeng/packi-template-tslib/archive/master.zip
     pki init tslib foobar
 `
 
